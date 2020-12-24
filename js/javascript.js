c818ff9b9c75adae381d11a17f6cd35e894ef3a9
Agony-C <76541531+Agony-C@users.noreply.github.com>
2020-12-24 09:40:16 +0800

    Add files via upload

---
+++ js/javascript.js
@@ -0,0 +1,55 @@
+var a=document.getElementById('x-l');
+var b=document.getElementById('wdtb');
+var c=document.getElementById('x-l2');
+var d=document.getElementById('gwc');
+var e=document.getElementById('x-l3');
+var f=document.getElementById('sc');
+var g=document.getElementById('x-l4');
+var h=document.getElementById('wyg');
+var i=document.getElementById('x-l5');
+var j=document.getElementById('mjzx');
+var k=document.getElementById('x-l6');
+var l=document.getElementById('wzdh');
+
+var out=document.getElementById('out');
+var img=document.getElementById('ewm');
+    out.onclick=function(){
+        img.style.display="none";
+    }
+    b.onmouseover=function () {
+        a.style.display="block";
+    }
+    b.onmouseout=function () {
+        a.style.display="none";
+    }
+    d.onmouseover=function () {
+        c.style.display="block";
+    }
+   d.onmouseout=function () {
+        c.style.display="none";
+    }
+    f.onmouseover=function () {
+        e.style.display="block";
+    }
+   f.onmouseout=function () {
+        e.style.display="none";
+    }
+    h.onmouseover=function () {
+        g.style.display="block";
+    }
+   h.onmouseout=function () {
+        g.style.display="none";
+    }
+    j.onmouseover=function () {
+        i.style.display="block";
+    }
+   j.onmouseout=function () {
+        i.style.display="none";
+    }
+    l.onmouseover=function () {
+        k.style.display="block";
+    }
+   l.onmouseout=function () {
+        k.style.display="none";
+    }
+
